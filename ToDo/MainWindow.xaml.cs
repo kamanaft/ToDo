9faaa491f9aace76f8606ca671b2484f3338a726
Alexey Zhukov <42152195+kamanaft@users.noreply.github.com>
2021-12-11 15:23:04 +0100

    Добавьте файлы проекта.

---
+++ ToDo/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ToDo.Models;
+using ToDo.Services;
+
+namespace ToDo
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Сохраняем в файл по этому пути в текущей директории
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\todoDataList.json";
+        private BindingList<ToDoModel> _todoDataList;
+        private FileIOService _fileIOService;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Loading + upload data from file
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            _fileIOService = new FileIOService(PATH);
+
+            try
+            {
+                _todoDataList = _fileIOService.LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+            
+           
+            dgToDoList.ItemsSource = _todoDataList;
+            _todoDataList.ListChanged += _todoDataList_ListChanged;
+
+        }
+
+        // Saving data if something was changed - events
+        private void _todoDataList_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            if (e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemChanged || e.ListChangedType == ListChangedType.ItemDeleted)
+            {
+                try
+                {
+                    _fileIOService.SaveData(sender);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    Close();
+
+                }
+            }            
+        }
+    }
+}
---
+++ ToDo/Models/ToDoModel.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.Models
+{
+    internal class ToDoModel: INotifyPropertyChanged
+    {
+        private bool _isDone;
+        private string _text;
+
+        [JsonProperty(PropertyName = "creationDate")]
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        // event changing data
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [JsonProperty(PropertyName = "isDone")]
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set 
+            { 
+                if (_isDone == value)
+                    return;
+                _isDone = value;
+                OnPropertyChanged("IsDone");
+            }
+        }
+        [JsonProperty(PropertyName = "text")]
+        public string Text
+        {
+            get { return _text; }
+            set 
+            { 
+                if (_text == value)
+                    return;
+                _text = value;
+                OnPropertyChanged("Text");
+            }
+        }   
+       
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
